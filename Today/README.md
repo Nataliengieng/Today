e2ba415d8c38d76fca3805cf985eaeba523509b4
Natalie Ng <ngcheokieng@yahoo.com.hk>
2023-11-28 16:18:55 +0000

    update README.md

--- Today/README.md
+++ Today/README.md
@@ -1,23 +1,3 @@
-# Scrumdinger App
+# Today App
 
-This app is developed based on the [Apple Developer Tutorials] (https://developer.apple.com/tutorials/app-dev-training).
-
-## TroubleShooting
-
-### 1. Handling errors
-When simulating data corruption following "Section 4: Simulate data corruption", error "xcrun: error: unable to find utility "simctl", not a developer tool or in PATH" occurs after running the command `xcrun simctl get_app_container booted com.example.apple-samplecode.Scrumdinger data`, and I were unable to accept the XCode license.
-
-```
-nat@Natalies-MacBook-Pro ~ % xcrun simctl get_app_container booted com.example.apple-samplecode.Scrumdinger data 
-xcrun: error: unable to find utility "simctl", not a developer tool or in PATH
-
-nat@Natalies-MacBook-Pro ~ % sudo xcodebuild -license
-Password:
-xcode-select: error: tool 'xcodebuild' requires Xcode, but active developer directory '/Library/Developer/CommandLineTools' is a command line tools instance
-```
-
-After troubleshooting, this was due to the location of the developer directory was not pointed to the correct directory. This problem was solved by switching the directory and accept the XCode license by running below commands: 
-```
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-sudo xcodebuild -license
-```
+This app is developed based on the [Apple Developer Tutorials - UIKit essentials](https://developer.apple.com/tutorials/app-dev-training/creating-a-list-view).
--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -7,11 +7,57 @@
 
 import UIKit
 
-class ReminderListViewController: UIViewController {
+class ReminderListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    var dataSource: DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+        
+        //specifies how to configure the content and appearance of a cell
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        var reminderTitles = [String]()
+        
+        /*
+        for reminder in Reminder.sampleData {
+            reminderTitles.append(reminder.title)
+        }
+        snapshot.appendItems(reminderTitles)
+        =
+        */
+        //  map returns a new array containing only the reminder titles, which populate as items in the snapshot
+        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+        //  applying the snapshot to the data source which reflects the changes in the user interface
+        dataSource.apply(snapshot)
+        
+        collectionView.dataSource = dataSource
+    }
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        //  create a new list configuration variable with the grouped appearance
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false //disable separators
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
     }
 
 
