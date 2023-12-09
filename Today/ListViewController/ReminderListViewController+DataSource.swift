2579f80c5e0a676fb7cba8c454c9d85b241f5b13
Natalie Ng <ngcheokieng@yahoo.com.hk>
2023-12-09 11:24:01 +0000

    complete "Loading reminders"

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -18,6 +18,9 @@ extension ReminderListViewController {
     var reminderNotCompletedValue: String {
         NSLocalizedString("Not completed", comment: "Reminder not completed value")
     }
+    
+    private var reminderStore: ReminderStore { ReminderStore.shared }
+    
     func updateSnapshot(reloading idsThatChanged: [Reminder.ID] = []) {
         let ids = idsThatChanged.filter { id in filteredReminders.contains(where: { $0.id == id })}
         var snapshot = Snapshot()
@@ -89,6 +92,22 @@ extension ReminderListViewController {
     func deleteReminder(withId id: Reminder.ID) {
         let index = reminders.indexOfReminder(withId: id)
         reminders.remove(at: index)
+    }
+    
+    func prepareReminderStore() {
+        Task {
+            do {
+                try await reminderStore.requestAccess()
+                reminders = try await reminderStore.readAll()
+            } catch TodayError.accessDenied, TodayError.accessRestricted {
+                #if DEBUG
+                reminders = Reminder.sampleData
+                #endif
+            } catch {
+                showError(error)
+            }
+            updateSnapshot()
+        }
     }
     
     private func doneButtonAccessibilityAction(for reminder: Reminder) -> UIAccessibilityCustomAction
--- Today/Models/TodayError.swift
+++ Today/Models/TodayError.swift
@@ -9,19 +9,31 @@ import Foundation
 
 enum TodayError: LocalizedError {
     
+    case accessDenied
+    case accessRestricted
     case failedReadingReminders
     case reminderHasNoDueDate
+    case unknown
     
     //  the default implementation of the errorDescription property that returns a non-specific message which allows users to have a clear information about why the error occur
     var errorDescription: String? {
         switch self {
+        case .accessDenied:
+            return NSLocalizedString(
+                "The app doesn't have permission to read reminders.",
+                comment: "access denied error description")
+        case .accessRestricted:
+            return NSLocalizedString(
+                "This device doesn't allow access to reminders.",
+                comment: "access restricted error description")
         case .failedReadingReminders:
             return NSLocalizedString(
                 "Failed to read reminders.", comment: "failed reading reminders error description")
         case .reminderHasNoDueDate:
             return NSLocalizedString(
                 "A reminder has no due date.", comment: "reminder has no due date error description")
-                
+        case .unknown:
+            return NSLocalizedString("An unknown error occurred.", comment: "unknown error description")
         }
     }
 }
